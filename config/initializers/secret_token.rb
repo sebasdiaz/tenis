a13820a5b5202cf6e94a50d131ccebd3c212f8a2
sebasdiaz <lksebas@gmail.com>
2011-07-16 23:58:32 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tenis::Application.config.secret_token = '39d52fd586c299edbea5e3d81eff87b13a1d2fb81817b1ef1925b7fe12f114a678d00351e393dfbd09d7ec0cb1f6d0e87d3bcc441d9cd35d7ee80dca3437fb5a'
